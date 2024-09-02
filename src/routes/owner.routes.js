2496d2d42eba4c9d9e62deef18c95833f6e5a81a
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-02 13:59:33 +0530

    Feat: added owner rout

--- src/routes/owner.routes.js
+++ src/routes/owner.routes.js
@@ -1,7 +1,24 @@
-import express from 'express'
-
+import express from "express";
+import Owner from "../models/owner.models.js";
 
 const router = express.Router();
 
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let owners = await Owner.find();
+    if (owners.length > 0) {
+      res.status(504).send({ message: "Owner already exists" });
+    } else {
+      const { fullName, email, password } = req.body;
+      const owner = new Owner({
+        fullName,
+        email,
+        password,
+      });
+      await owner.save();
+      res.status(201).send({ message: "Owner created successfully" });
+    }
+  });
+}
 
-export default router
+export default router;
