5af9ed8245779891f47df3b97d75a4755270af98
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-02 13:37:53 +0530

    added log

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDatabase = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-
-export default connectDatabase
+export default connectDatabase;
