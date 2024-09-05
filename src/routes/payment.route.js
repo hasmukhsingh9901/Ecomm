afdc8699b0b87888a742db6d6ea323e6e22da5de
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-05 18:43:29 +0530

    payment route and controller

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,13 +1,18 @@
-import express from 'express';
-
-
+import express from "express";
+import { paymentController } from "../controllers/payment.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express();
 
+router.post(
+  "/create-checkout-session",
+  protectRoute,
+  paymentController.createCheckoutSession
+);
+router.post(
+  "/checkout-success",
+  protectRoute,
+  paymentController.checkoutSuccess
+);
 
-
-
-
-
-
-export default router
+export default router;
