bff6d3c9c5c25494f885249b6dec988d654ed3bb
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-05 12:37:44 +0530

    Cart routes & controller

--- src/index.js
+++ src/index.js
@@ -8,23 +8,20 @@ import path from "path";
 import morgan from "morgan";
 
 // routes
-import authRoutes from "./routes/auth.route.js"
-import productRoutes from "./routes/product.route.js"
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 // Utility functions to get the current file path and connect to the database
 import { fileURLToPath } from "url";
 import { connectDB } from "./lib/db.js";
 
-
 dotenv.config({ path: "./.env" });
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 dotenv.config();
-
 
 const app = express();
 
@@ -39,26 +36,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
 
-
 app.get("/", (req, res) => {
-    res.send("Welcome to the server of CarryAll 🌿");
+  res.send("Welcome to the server of CarryAll 🌿");
 });
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 // Configure Cloudinary with environment variables
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
