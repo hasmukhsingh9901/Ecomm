834aec7d0deb256615ae98001f6a0c36ce6391a9
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-11 21:10:36 +0530

    updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.mode === "development" ? "https://localhost:8000/api" : "/api",
+  // baseURL:
+  //   import.meta.mode === "development" ? "https://localhost:8000/api" : "/api",
+  baseURL:"https://carryall.onrender.com/api",
   withCredentials: true,
 });
 
