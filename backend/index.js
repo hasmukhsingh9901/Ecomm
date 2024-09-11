7374a2c457f20a567ea1594e116f13e855b8ac01
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-11 21:41:51 +0530

    corsoptions

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,19 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+const corsOptions = {
+	origin: 'https://carry-all.vercel.app', // replace with your front-end domain
+	methods: ['GET', 'POST', 'PUT', 'DELETE'], // allowed methods
+	allowedHeaders: ['Content-Type', 'Authorization'], // allowed headers
+	credentials: true, // allow credentials (cookies, authorization headers, etc.)
+  };
+  
+
+
 app.use(express());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
