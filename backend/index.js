9712ee7f6e84a9076412e9f74f562fd7604e345b
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-11 11:29:26 +0530

    production route updated

--- backend/index.js
+++ backend/index.js
@@ -41,10 +41,10 @@ app.get("/", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
 
-	app.use(express.static(path.join(__dirname, "/client/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
 
